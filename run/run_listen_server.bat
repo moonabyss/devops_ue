f5957586592428f78e5f8c819f48cb8b0e7ac205
moonabyss <d-v@ukr.net>
2023-07-03 01:28:22 +0300

    fix incomplete string

--- run/run_listen_server.bat
+++ run/run_listen_server.bat
@@ -1,6 +1,6 @@
 :: Copyright moonabyss. All Rights Reserved.
 @echo off
 
-ops_data\config.bat"
+call "%~dp0..\..\devops_data\config.bat"
 
 start "" "%GameExePath%" ?listen -WINDOWED -ResX=600 -ResY=600 -WinX=1230 -WinY=50
