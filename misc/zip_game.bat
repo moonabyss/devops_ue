b1b4056b3b6e0b4d9a9d78cbcf71423ec4367858
moonabyss <d-v@ukr.net>
2023-07-07 19:13:56 +0300

    zip_game.bat

---
+++ misc/zip_game.bat
@@ -0,0 +1,7 @@
+:: Copyright moonabyss. All Rights Reserved.
+@echo off
+
+call "%~dp0..\devops_data\config.bat"
+
+del %ProjectPureName%.rar
+%RarPath% a %ProjectPureName%.rar Windows
--- package/package_game.bat
+++ package/package_game.bat
@@ -3,9 +3,13 @@
 
 call "%~dp0..\..\devops_data\config.bat"
 
+rmdir /s /q "%ArchivePath%\Windows"
+
 "%RunUATPath%" BuildCookRun ^
 -project="%ProjectPath%" ^
 -platform="%Platform%" ^
 -clientconfig="%Configuration%" ^
 -archivedirectory="%ArchivePath%" ^
 -build -cook -package -stage -archive -pak -allmaps
+
+copy /y "..\Misc\zip_game.bat" "%ArchivePath%
